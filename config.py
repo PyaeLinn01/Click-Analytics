9ae94aa7fadf4cb945cb7c88e9c127ed6008aa98
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-05-02 18:48:06 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import streamlit as st
+
+def set_page_config():
+    page_icon = Image.open("./assets/logo.png")
+    st.set_page_config(layout="centered", page_title="Click Analyst", page_icon=page_icon)
