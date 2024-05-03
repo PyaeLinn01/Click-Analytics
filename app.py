77ea1345dec65428660eede319e67c580ddf4401
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-05-03 09:38:44 +0630

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ import eda_module
 from missing_values_handler import handle_missing_values
 from CTGD import handle_categorical_data
 from scaling_functions import display_scaling_options
+from transformation_functions import display_transformation_options
+
 
 # Set configuration and initialize state
 set_page_config()
@@ -148,107 +150,7 @@ if st.session_state.df is not None:
 
 
     # Data Transformation
-    new_line()
-    st.markdown("### 🧬 Data Transformation", unsafe_allow_html=True)
-    new_line()
-    with st.expander("Show Data Transformation"):
-        new_line()
-        
-
-
-        # Transformation Methods
-        trans_methods = st.checkbox("Explain Transformation Methods", key="trans_methods", value=False)
-        if trans_methods:
-            new_line()
-            col1, col2, col3, col4 = st.columns(4)
-            with col1:
-                st.markdown("<h6 align='center'> Log <br> Transformation</h6>", unsafe_allow_html=True)
-                st.latex(r'''z = log(x)''')
-
-            with col2:
-                st.markdown("<h6 align='center'> Square Root Transformation </h6>", unsafe_allow_html=True)
-                st.latex(r'''z = \sqrt{x}''')
-
-            with col3:
-                st.markdown("<h6 align='center'> Cube Root Transformation </h6>", unsafe_allow_html=True)
-                st.latex(r'''z = \sqrt[3]{x}''')
-
-            with col4:
-                st.markdown("<h6 align='center'> Exponential Transformation </h6>", unsafe_allow_html=True)
-                st.latex(r'''z = e^x''')
-
-
-
-        # INPUT
-        new_line()
-        col1, col2 = st.columns(2)
-        with col1:
-            trans_feat = st.multiselect("Select Features", df.select_dtypes(include=np.number).columns.tolist(), help="Select the features you want to transform.", key="transformation features")
-
-        with col2:
-            trans = st.selectbox("Select Transformation", ["Select", "Log Transformation", "Square Root Transformation", "Cube Root Transformation", "Exponential Transformation"],
-                                  help="Select the transformation you want to apply.", 
-                                  key= "transformation")
-        
-
-        if trans_feat and trans != "Select":
-            new_line()
-            col1, col2, col3 = st.columns([1, 0.5, 1])
-            if col2.button("Apply", key='trans_apply',use_container_width=True ,help="Click to apply transformation."):
-
-                progress_bar()
-
-                # new_line()
-                # Log Transformation
-                if trans == "Log Transformation":
-                    st.session_state.all_the_process += f"""
-#Log Transformation
-df[{trans_feat}] = np.log1p(df[{trans_feat}])
-\n """
-                    df[trans_feat] = np.log1p(df[trans_feat])
-                    st.session_state['df'] = df
-                    st.success("Numerical features have been transformed using Log Transformation.")
-
-                # Square Root Transformation
-                elif trans == "Square Root Transformation":
-                    st.session_state.all_the_process += f"""
-#Square Root Transformation
-df[{trans_feat}] = np.sqrt(df[{trans_feat}])
-\n """
-                    df[trans_feat] = np.sqrt(df[trans_feat])
-                    st.session_state['df'] = df
-                    st.success("Numerical features have been transformed using Square Root Transformation.")
-
-                # Cube Root Transformation
-                elif trans == "Cube Root Transformation":
-                    st.session_state.all_the_process += f"""
-#Cube Root Transformation
-df[{trans_feat}] = np.cbrt(df[{trans_feat}])
-\n """
-                    df[trans_feat] = np.cbrt(df[trans_feat])
-                    st.session_state['df'] = df
-                    st.success("Numerical features have been transformed using Cube Root Transformation.")
-
-                # Exponential Transformation
-                elif trans == "Exponential Transformation":
-                    st.session_state.all_the_process += f"""
-#Exponential Transformation
-df[{trans_feat}] = np.exp(df[{trans_feat}])
-\n """
-                    df[trans_feat] = np.exp(df[trans_feat])
-                    st.session_state['df'] = df
-                    st.success("Numerical features have been transformed using Exponential Transformation.")
-
-        # Show DataFrame Button
-        # new_line()
-        col1, col2, col3 = st.columns([0.15,1,0.15])
-        col2.divider()
-        col1, col2, col3 = st.columns([0.9, 0.6, 1])
-        with col2:
-            show_df = st.button("Show DataFrame", key="trans_show_df", help="Click to show the DataFrame.")
-        
-        if show_df:
-            st.dataframe(df, use_container_width=True)
+    display_transformation_options(st, df)
 
 
     # Feature Engineering
