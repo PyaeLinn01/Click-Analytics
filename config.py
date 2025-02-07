4abe668875844c87a3fcb0954981865fcc2b6aff
PyaeLinn01 <150521896+PyaeLinn01@users.noreply.github.com>
2025-02-08 00:15:01 +0630

    config

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
 from PIL import Image
 import streamlit as st
 
 def set_page_config():
-    page_icon = Image.open("assets/logo.png")
+    # Construct the path to the logo.png file in the assets folder
+    logo_path = os.path.join("assets", "logo.png")
+    page_icon = Image.open(logo_path)
     st.set_page_config(layout="centered", page_title="Click Analyst", page_icon=page_icon)
+
+# Call the function
+set_page_config()
